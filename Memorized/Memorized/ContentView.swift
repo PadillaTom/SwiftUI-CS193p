85af8ff11e0373ae40e506bcc9b6996547876eb1
Tomas Padilla <padillatomasagustin@gmail.com>
2022-12-05 08:56:01 +0100

    [develop] Add Cards and Buttons

--- Memorized/Memorized/ContentView.swift
+++ Memorized/Memorized/ContentView.swift
@@ -8,30 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["✈️","🚝","🚁","🚗", "🚕", "🚌", "🚚", "🚎", "🏎️", "🚓", "🛺", "🚔", "🚑", "🚒", "🚐", "🚙", "🚘", "🚍", "🚃", "🛸", "🚀", "🛳️", "⛵️", "🛶"]
+    @State var emojiCount = 4
     var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack{
+            HStack {
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(cardContent: emoji)
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+                removeBtn
+                Spacer()
+                addBtn
+            }
+            .padding(.horizontal)
+            .font(.largeTitle)
         }
         .padding(.horizontal)
-        .foregroundColor(.red)
+    }
+    
+    var removeBtn: some View {
+        Button(action: {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        }, label: {
+           Image(systemName: "minus.circle")
+        })
+    }
+    
+    var addBtn: some View {
+        Button(action: {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        }, label: {
+            Image(systemName: "plus.circle")
+        })
     }
 }
 
 struct CardView: View {
+    var cardContent: String
+    @State var isFaceUp: Bool = true
+    
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .fill()
-                .foregroundColor(.white)
+            let cardShape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                cardShape.fill().foregroundColor(.white)
+                cardShape.stroke(lineWidth: 3)
+                Text(cardContent).font(.largeTitle)
+            } else {
+                cardShape.fill(.red)
+            }
             
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            
-            Text("✈️")
-                .font(.largeTitle)
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
         }
     }
 }
